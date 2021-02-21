d8c3c3193f20259f5af006068c6a90eda859147e
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-21 14:06:55 +0100

    imported couple of components from react-admin

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  List,
+  Datagrid,
+  TextField,
+  DateField,
+  EditButton,
+  DeleteButton,
+} from 'react-admin';
 
 const PostList = () => {
   return <div></div>;
