fe7a579e375591da5c69c75e5ed9a88285b4b8bc
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-22 12:35:42 +0100

    get props

--- client/src/components/PostCreate.js
+++ client/src/components/PostCreate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Create, SimpleForm, TextInput, DateInput } from 'react-admin';
 
-const PostCreate = () => {
+const PostCreate = (props) => {
   return <div></div>;
 };
 
