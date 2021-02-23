0ce8ec904d570e5a62f99b03ebcb0355272bb3f7
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-23 15:16:37 +0100

    render SimpleForm

--- client/src/components/UserCreate.js
+++ client/src/components/UserCreate.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Create, SimpleForm, TextInput } from 'react-admin';
 
 const UserCreate = () => {
-  return <Create></Create>;
+  return (
+    <Create>
+      <SimpleForm></SimpleForm>
+    </Create>
+  );
 };
 
 export default UserCreate;
