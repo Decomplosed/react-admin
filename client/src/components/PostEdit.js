75a0bb46c3c008ed12e6313fea0e74cc5f1f093b
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-23 15:01:16 +0100

    use Edit instead of Create

--- client/src/components/PostEdit.js
+++ client/src/components/PostEdit.js
@@ -3,13 +3,13 @@ import { Edit, SimpleForm, TextInput, DateInput } from 'react-admin';
 
 const PostEdit = (props) => {
   return (
-    <Create title='Create a Post...' {...props}>
+    <Edit title='Edit a Post...' {...props}>
       <SimpleForm>
         <TextInput source='title' />
         <TextInput source='body' multiline />
         <DateInput label='Published' source='publishedAt' />
       </SimpleForm>
-    </Create>
+    </Edit>
   );
 };
 
