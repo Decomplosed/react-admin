890228b4c6c0ec543a66faf507eb010c92a00d00
Bart Frackiewicz <decomplosed@gmail.com>
2021-02-23 15:22:34 +0100

    render SimpleForm

--- client/src/components/UserEdit.js
+++ client/src/components/UserEdit.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Edit, SimpleForm, TextInput } from 'react-admin';
 
 const UserEdit = () => {
-  return <Edit></Edit>;
+  return (
+    <Edit>
+      <SimpleForm></SimpleForm>
+    </Edit>
+  );
 };
 
 export default UserEdit;
